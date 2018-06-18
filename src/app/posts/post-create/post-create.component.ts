d94d5075a465e65d1e7a7362cb78b1d59bddabfa
Devon Fazekas <thomas-d@hotmail.ca>
2018-06-18 14:15:04 -0400

    Finished add-post feature.

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 // By adding a TS Decorator to our TS class, Angular uses it as a Component.
 // The Component Decorator takes some configuration in the form of a JS object.
@@ -8,14 +8,23 @@ import { Component } from "@angular/core";
     selector: 'app-post-create',
     // Point the HTML file to get the template.
     templateUrl: './post-create.component.html',
+    // Point to the CSS file to get the styles.
     styleUrls: ['./post-create.component.css']
 })
 
 export class PostCreateComponent {
-    enteredValue = "";
-    newPost = "No content";
+    enteredTitle = "";
+    enteredContent = "";
+    // An event that be listened to by parent component.
+    @Output() postCreated = new EventEmitter();
 
     onAddPost() {
-        this.newPost = this.enteredValue;    
+        const post = {
+            title: this.enteredTitle,
+            content: this.enteredContent
+        };
+        // Emit our newly created Post for the post-list 
+        // component to receive.
+        this.postCreated.emit(post);    
     }
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: "app-post-list",
@@ -8,9 +8,12 @@ import { Component } from "@angular/core";
 })
 
 export class PostListComponent {
-    posts = [
+    //posts = [
         // { title: "First post", content: "This is the first post's content." },
         // { title: "Second post", content: "This is the second post's content." },
         // { title: "Third post", content: "This is the third post's content." }
-    ];
+    //];
+
+    // Makes posts bindable from parent component.
+    @Input() posts = [];
 }
